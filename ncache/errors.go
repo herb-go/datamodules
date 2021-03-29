9be8d295920ba7761acf220e3292dfeaaaa8b194
jarlyyn <jarlyyn@qq.com>
2021-03-30 02:27:53 +0800

    update

--- ncache/errors.go
+++
@@ -1,8 +0,0 @@
-package ncache
-
-import "errors"
-
-var ErrUnresolvableCacheEnity = errors.New("ncache unresolvable cache enity")
-var ErrEnityTypecodeNotMatch = errors.New("ncache enity typecode not match")
-var ErrEnityVersionNotMatch = errors.New("ncache enity version not match")
-var ErrNoVersionStore = errors.New("ncache no version store")
