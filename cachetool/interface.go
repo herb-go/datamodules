b5e53ccbb26a8f8c331259674e075b7ff4bbf1da
jarlyyn <jarlyyn@qq.com>
2021-04-08 00:59:37 +0800

    update

---
+++ cachetool/interface.go
@@ -0,0 +1,38 @@
+package cachetool
+
+import (
+	"time"
+
+	"github.com/herb-go/herbdata/dataencoding/msgpackencoding"
+
+	"github.com/herb-go/herbdata/dataencoding"
+	"github.com/herb-go/misc/waitingmap"
+
+	"github.com/herb-go/herbdata"
+)
+
+type Loader interface {
+	Load(key []byte) error
+}
+
+type Saver interface {
+	Save(key []byte) error
+}
+
+type LoaderSaver interface {
+	Loader
+	Saver
+}
+
+var DefaultEncoding = msgpackencoding.Encoding
+
+type Settings struct {
+	TTL       time.Duration
+	Cache     herbdata.Cache
+	LockerMap *waitingmap.LockerMap
+	Encoding  *dataencoding.Encoding
+}
+
+func NewSettings() *Settings {
+	return &Settings{}
+}
